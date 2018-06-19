c007c7ca2f01dba932a4c58f2576b15aa7fe3f96
Dumi <alphnozoalph@gmail.com>
2018-06-19 09:03:32 +0300

    testing

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Gmex.Mixfile do
     [
       app: :gmex,
       version: "0.1.0",
-      elixir: "~> 1.2",
+      elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
       package: package(),
