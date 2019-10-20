1641d5d2d25e675d1e1cfeb6e21fe327dccaf2da
slash <webmaster@slashpanda.com>
2019-10-21 01:26:41 +0200

    Update ex_doc

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule Gmex.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-        {:earmark, "~> 0.1", only: :dev},
-        {:ex_doc, "~> 0.11", only: :dev},
+        {:earmark, "~> 1.4", only: :dev},
+        {:ex_doc, "~> 0.19", only: :dev},
         {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
